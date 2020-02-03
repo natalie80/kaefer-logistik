5513593c9be2fdafc695da8d18f0fd99d272c5d8
natali.kaefer <natalikaefer@hotmail.de>
2020-02-03 11:56:15 +0100

    conversion from class components to hooks functional components the file layout

--- logistik-app/src/hoc/Layout/Layout.js
+++ logistik-app/src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import {connect} from 'react-redux';
 import Media from "react-media";
 import { withRouter } from "react-router-dom";
@@ -9,57 +9,55 @@ import Footer from '../../pages/Footer/Footer';
 import NavigationDesktop from '../../components/molecules/Navigation/NavigationDesktop/NavigationDesktop';
 import SideDrawer from '../../components/molecules/Navigation/SideDrawerMobile/Sidedrawer';
 
-class Layout extends Component {
-    state = {
-        showMobileNavigation: false
+const Layout = (props) => {
+    const [ showMobileNavigation, setShowMobileNavigation ] = useState(false);
+
+    const mobileNavigationClosedHandler = () => {
+       // this.setState({showMobileNavigation: false})
+        setShowMobileNavigation(false);
     };
     
-    mobileNavigationClosedHandler = () => {
-        this.setState({showMobileNavigation: false})
-    };
-    
-    mobileNavigationToggleHandler = () => {
-        this.setState((prevState) => {
-            return { showMobileNavigation: !prevState.showMobileNavigation }
+    const mobileNavigationToggleHandler = () => {
+        setShowMobileNavigation((prevState) => {
+            return  !prevState.showMobileNavigation
         })
     };
-    
-    render () {
-        const url = this.props.location.pathname.split('/');
 
-        return (
-            <React.Fragment>
+        const url = props.location.pathname.split('/');
 
-                <Media
-                    queries={{
-                        small: "(max-width: 599px)",
-                        medium: "(min-width: 600px) and (max-width: 1199px)",
-                        large: "(min-width: 1200px)"
-                    }}
-                >
-                    { matches => (
-                        <div className={
-                            (url[1] === '' || url[1] === 'protection') ? 'Content_Homepage'
-                                : (matches.large || matches.medium) ? "Content"
-                                : matches.small ?  "Content_Mobile"
-                                    : null
-                        }>
-                            <header className="Header">
-                                <Logo />
-                                <NavigationDesktop toggleHandler={this.mobileNavigationToggleHandler}  isAuth={this.props.isAuthenticated}/>
-                            </header>
-                            <SideDrawer closed={this.mobileNavigationClosedHandler}  open={this.state.showMobileNavigation} isAuth={this.props.isAuthenticated} />
-                            <main className={matches.large ? "Main" : (matches.medium || matches.small) ?  "Main_Mobile" : null } >{this.props.children}</main>
-                            <footer className="Footer">
-                                <Footer isAuth={this.props.isAuthenticated}/>
-                            </footer>
-                        </div>
-                    )}
-                </Media>
-            </React.Fragment>
-        );
-    }
-}
+    return (
+        <React.Fragment>
+
+            <Media
+                queries={{
+                    small: "(max-width: 599px)",
+                    medium: "(min-width: 600px) and (max-width: 1199px)",
+                    large: "(min-width: 1200px)"
+                }}
+            >
+                { matches => (
+                    <div className={
+                        (url[1] === '' || url[1] === 'protection') ? 'Content_Homepage'
+                            : (matches.large || matches.medium) ? "Content"
+                            : matches.small ?  "Content_Mobile"
+                                : null
+                    }>
+                        <header className="Header">
+                            <Logo />
+                            <NavigationDesktop toggleHandler={mobileNavigationToggleHandler}  isAuth={props.isAuthenticated}/>
+                        </header>
+                        <SideDrawer closed={mobileNavigationClosedHandler}  open={showMobileNavigation} isAuth={props.isAuthenticated} />
+                        <main className={matches.large ? "Main" : (matches.medium || matches.small) ?  "Main_Mobile" : null } >{props.children}</main>
+                        <footer className="Footer">
+                            <Footer isAuth={props.isAuthenticated}/>
+                        </footer>
+                    </div>
+                )}
+            </Media>
+
+        </React.Fragment>
+    );
+};
 
 const mapStateToProps = state => {
     return {
