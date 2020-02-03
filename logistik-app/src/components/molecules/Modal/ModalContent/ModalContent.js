e2133c1467ca6a86f28757ed2d33df259948a46a
natali.kaefer <natalikaefer@hotmail.de>
2020-02-03 11:45:02 +0100

    conversion from class components to hooks functional components the file modal

--- logistik-app/src/components/molecules/Modal/ModalContent/ModalContent.js
+++ logistik-app/src/components/molecules/Modal/ModalContent/ModalContent.js
@@ -1,4 +1,4 @@
-import  React, {Component} from 'react';
+import  React from 'react';
 import FocusTrap from "focus-trap-react";
 
 import './ModalContent.scss';
@@ -6,45 +6,44 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faWindowClose} from "@fortawesome/free-solid-svg-icons";
 import Media from "react-media";
 
-export class ModalContent extends Component {
-    render() {
-        return (
-            <FocusTrap>
-                <Media
-                    queries={{
-                        small: "(max-width: 599px)",
-                        medium: "(min-width: 600px) and (max-width: 1199px)",
-                        large: "(min-width: 1200px)"
-                    }}
-                >
-                    { matches => (
-                        <div
-                            role="dialog"
-                            tabIndex="-1"
-                            aria-modal="true"
-                            className="ModalCover"
-                            onClick={this.props.onClickOutside}
-                            onKeyDown={this.props.onKeyDown}
-                        >
-                            <div className= { (matches.large || matches.medium)? "ModalArea" :  matches.small ?  "ModalArea_Mobile" : null }  ref={this.props.modalRef}>
-                                <button
-                                    ref={this.props.buttonRef}
-                                    aria-label="Close Modal"
-                                    aria-labelledby="close-modal"
-                                    className="ModalCloseButton"
-                                    onClick={this.props.closeModal}
-                                >
-                                    <FontAwesomeIcon icon={faWindowClose} color="gray"  className="fa-2x"/>
-                                </button>
-                                <div className="ModalContent">{this.props.content}</div>
-                            </div>
+const ModalContent = (props) => {
+
+    return (
+        <FocusTrap>
+            <Media
+                queries={{
+                    small: "(max-width: 599px)",
+                    medium: "(min-width: 600px) and (max-width: 1199px)",
+                    large: "(min-width: 1200px)"
+                }}
+            >
+                { matches => (
+                    <div
+                        role="dialog"
+                        tabIndex="-1"
+                        aria-modal="true"
+                        className="ModalCover"
+                        onClick={props.onClickOutside}
+                        onKeyDown={props.onKeyDown}
+                    >
+                        <div className= { (matches.large || matches.medium)? "ModalArea" :  matches.small ?  "ModalArea_Mobile" : null }  ref={props.modalRef}>
+                            <button
+                                ref={props.buttonRef}
+                                aria-label="Close Modal"
+                                aria-labelledby="close-modal"
+                                className="ModalCloseButton"
+                                onClick={props.closeModal}
+                            >
+                                <FontAwesomeIcon icon={faWindowClose} color="gray"  className="fa-2x"/>
+                            </button>
+                            <div className="ModalContent">{props.content}</div>
                         </div>
-                    )}
-                </Media>
-            </FocusTrap>
-          
-        );
-    }
+                    </div>
+                )}
+            </Media>
+        </FocusTrap>
+
+    );
 }
 
 export default ModalContent;
