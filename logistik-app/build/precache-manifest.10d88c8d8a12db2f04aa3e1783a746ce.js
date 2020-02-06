58f44124980c6fb6df6c79c3e46ceaafff3d92a4
natali.kaefer <natalikaefer@hotmail.de>
2020-02-06 16:41:59 +0100

    test 22 commit

---
+++ logistik-app/build/precache-manifest.10d88c8d8a12db2f04aa3e1783a746ce.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbcffeb2720238451cb77865e4260a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a507ab3912d937f8401",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "304c07784df1134d360a",
+    "url": "/static/css/main.e0d7d518.chunk.css"
+  },
+  {
+    "revision": "4a507ab3912d937f8401",
+    "url": "/static/js/2.2e18ede5.chunk.js"
+  },
+  {
+    "revision": "43e0eed6b58ee2bb2f8077df1fa0b838",
+    "url": "/static/js/2.2e18ede5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "304c07784df1134d360a",
+    "url": "/static/js/main.e470988e.chunk.js"
+  },
+  {
+    "revision": "9527c24c64217ddb6d98",
+    "url": "/static/js/runtime-main.c3dcc7ee.js"
+  },
+  {
+    "revision": "0823f354e124c081383df10b532d5bed",
+    "url": "/static/media/Default_Image.0823f354.png"
+  },
+  {
+    "revision": "a32839f53c063c614448946aeec2fae7",
+    "url": "/static/media/container_1.a32839f5.jpg"
+  },
+  {
+    "revision": "97895df0c6c0ec0e1f72bf1caa01c192",
+    "url": "/static/media/digitization_1.97895df0.jpg"
+  },
+  {
+    "revision": "7a50986990326bc2920c14f4da1d9472",
+    "url": "/static/media/logistics_1.7a509869.jpg"
+  }
+]);
